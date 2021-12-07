8b2f49dd301113bff3259670789db9f96afc7058
anusoruban <34101007+anusasharma@users.noreply.github.com>
2021-12-07 19:40:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const path = require("path");
 
 const app = express()
 
-app.use(express.static('./dist/Survey-site'));
+app.use(express.static(__dirname + './dist/Survey-site'));
 
-app.get('/*', (req, res) =>
-     res.sendFile("index.html", {root:'dist/Survey-site/'})
+app.get('*', (req, res) =>
+     res.sendFile(path.join(__dirname + 'dist/Survey-site/index.html'))
         
 app.listen(process.env.PORT || 8080));
