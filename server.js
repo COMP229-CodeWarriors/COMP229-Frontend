787482ed3825a02098574a365cd3426fa9ea07c5
anusoruban <34101007+anusasharma@users.noreply.github.com>
2021-12-07 19:35:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ const app = express()
 app.use(express.static('./dist/Survey-site'));
 
 app.get('/*', (req, res) =>
-     res.sendFile("index.html", {root:'dist/angular-heroku/'})
+     res.sendFile("index.html", {root:'dist/Survey-site/'})
         
 app.listen(process.env.PORT || 8080));
